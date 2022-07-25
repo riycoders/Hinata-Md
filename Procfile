aad66b6853b215cc14b554863bfbc23f4f5268d4
Your Name <riychdwaynee@gmail.com>
2022-07-26 01:16:15 +0700

    V3

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: node . -- db 'mongodb+srv://hisoka:hisokamorrow@hisoka.axmu8.mongodb.net/?retryWrites=true&w=majority'
---
+++ settings.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Free Apikey
+global.APIs = {
+	riy: 'https://api-xriy04.herokuapp.com',
+}
+
+// Other
+global.owner = ['6281575886399']
+global.premium = ['6281575886399']
+global.youtube = 'https://youtube.com/c/Riy'
+global.ownername = 'Riy'
+global.botname ='Hinata-Md'
+global.donasi = {
+    saweria: 'https://saweria.co/riy20',
+    nomor: '081575886399'
+}
+global.packname = 'Hinata-Md'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...'
+}
+global.thumb = fs.readFileSync('./image/hinata.jpg')
+global.vn = './sound/menu.mp3'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
